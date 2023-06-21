4e5ccad45da6d35748352bdc799a503642c93116
ankit <ankit_m@ph.iitr.ac.in>
2023-06-21 09:53:17 +0530

    nomralizeURL and tests

---
+++ crawl.js
@@ -0,0 +1,16 @@
+function normalizeURL(urlString){
+  
+  const urlobj = new URL(urlString);
+  const hostpath = `${urlobj.hostname}${urlobj.pathname}`
+  if(hostpath.length > 0 && hostpath.slice(-1) === '/'){
+     return hostpath.slice(0,-1);
+  }
+
+  return  hostpath;
+}
+
+
+
+module.exports = {
+    normalizeURL
+}
